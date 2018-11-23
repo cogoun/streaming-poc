98fe4fe19ab6f732db9b85b0fe0e0ef6062901d7
Konstantinos Gkountanas <konstantinos.gkountanas@trasys.gr>
2018-11-23 17:25:54 +0200

    Added some more features

--- streaming-integration-testing-tool/src/test/java/com/cogoun/streaming/tests/load/HighLoadIntegrationTest.java
+++ streaming-integration-testing-tool/src/test/java/com/cogoun/streaming/tests/load/HighLoadIntegrationTest.java
@@ -2,13 +2,13 @@ package com.cogoun.streaming.tests.load;
 
 import org.junit.Test;
 
-public class HighLoad {
+public class HighLoadIntegrationTest {
 
     private static final String ENDPOINT_URL = "http://localhost:8092/collaboration";
 
     @Test
     public void testHighLoad() {
-        LoadTest highLoadTest = new EmulateHighLoad(100, 200);
+        LoadTest highLoadTest = new EmulateHighLoad(1000, 200);
         highLoadTest.test(ENDPOINT_URL);
     }
 }
--- task-service-app/src/main/java/com/cogoun/streaming/task/TaskRepository.java
+++ task-service-app/src/main/java/com/cogoun/streaming/task/TaskRepository.java
@@ -2,8 +2,9 @@ package com.cogoun.streaming.task;
 
 import com.cogoun.streaming.domain.Task;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends ElasticsearchRepository<TaskEntity, String> {
+public interface TaskRepository extends PagingAndSortingRepository<TaskEntity, String> {
 }
